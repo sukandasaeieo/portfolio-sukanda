979a542f056e10d0e6c81dd23a7d2a437acb3eb0
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-21 23:36:12 +0700

    add image

--- src/components/project/projectarray.jsx
+++ src/components/project/projectarray.jsx
@@ -5,6 +5,10 @@ import image4_Crowdfunding from '../../image/projects/Crowdfunding product page.
 import image5_Insure from '../../image/projects/Insure landing page.png'
 import image6_Huddle from '../../image/projects/Huddle landing page.png'
 import image7_Fylodarktheme from '../../image/projects/Fylo dark theme landing page.png'
+import image8_Colma from '../../image/projects/Colmar-Academy.png'
+import image9_Findyourhat from '../../image/projects/findyourhat.gif'
+import image10_ReactAssessmet from '../../image/projects/ReactAssessmet.gif'
+
 
 export const projectarray = [
     {   id:1 , 
@@ -12,6 +16,7 @@ export const projectarray = [
         stack:['HTML','CSS'],
         responsive:['Desktop' ,'Mobile'],
         image:image1_Sunnyside ,
+        isgif:false,
         repoGithub:'https://github.com/Siraphob1/frontend_mentor/tree/main/Challange7_sunnyside-agency-landing-page-main' ,
         linksite:'https://6472d53ace7473732e3ac545--mellow-parfait-2a0095.netlify.app/'
     },
@@ -20,6 +25,7 @@ export const projectarray = [
         stack:['ReactJS' ,'Vite' ,'Tailwind'],
         responsive:['Desktop' ,'Mobile'],
         image:image2_Loopstudios ,
+        isgif:false,
         repoGithub:'https://github.com/Siraphob1/Loopstudios-landing-page' ,
         linksite:'https://648805f21652ff6534d76ff7--inquisitive-queijadas-07563e.netlify.app/'
     },
@@ -28,6 +34,7 @@ export const projectarray = [
         stack:['ReactJS' ,'Vite' ,'Tailwind'],
         responsive:['Desktop' ,'Mobile'],
         image:image3_Clipboard ,
+        isgif:false,
         repoGithub:'https://github.com/Siraphob1/Clipboard-landing-page-master' ,
         linksite:'https://64830d95c5099d1475877c86--jade-paprenjak-0fd99e.netlify.app/'
     },
@@ -36,6 +43,7 @@ export const projectarray = [
         stack:['ReactJS' ,'Vite' ,'Tailwind'],
         responsive:['Desktop' ,'Mobile'],
         image:image4_Crowdfunding ,
+        isgif:false,
         repoGithub:'https://github.com/Siraphob1/Crowdfunding-product-page-main' ,
         linksite:'https://64816af96204ab2600d12ed2--tranquil-sprinkles-7bc803.netlify.app/'
     },
@@ -44,6 +52,7 @@ export const projectarray = [
         stack:['ReactJS' ,'Vite' ,'Tailwind'],
         responsive:['Desktop' ,'Mobile'],
         image:image5_Insure ,
+        isgif:false,
         repoGithub:'https://github.com/Siraphob1/Insure-landing-page-master' ,
         linksite:'https://64898651aa90f2008a1bedfb--idyllic-hotteok-9362b9.netlify.app/'
     },
@@ -52,6 +61,7 @@ export const projectarray = [
         stack:['ReactJS' ,'Vite' ,'Tailwind'],
         responsive:['Desktop' ,'Mobile'],
         image:image6_Huddle,
+        isgif:false,
         repoGithub:'https://github.com/Siraphob1/Huddle' ,
         linksite:'https://648c2c841ebecc529add5c81--hilarious-sfogliatella-7cd043.netlify.app/'
     },
@@ -60,7 +70,35 @@ export const projectarray = [
         stack:['ReactJS' ,'Vite' ,'Tailwind'],
         responsive:['Desktop' ,'Mobile'],
         image:image7_Fylodarktheme,
+        isgif:false,
         repoGithub:'https://github.com/Siraphob1/Fylo-dark-theme-landing-page' ,
         linksite:'https://64956d1ee032e5146a130471--polite-sfogliatella-bfaffd.netlify.app/'
+    },
+    {   id: 8, 
+        topic:'Colmar Academy',
+        stack:['HTML','CSS'],
+        responsive:['Desktop' ,'Mobile'],
+        image:image8_Colma,
+        isgif:false,
+        repoGithub:'https://github.com/Siraphob1/Colmar-Academy' ,
+        linksite:'https://colmar-academy-7252t2ua5-siraphob1.vercel.app/'
+    },
+    {   id: 9, 
+        topic:'Find your hat',
+        stack:['JS'],
+        responsive:['Desktop'],
+        image:image9_Findyourhat,
+        isgif:true,
+        repoGithub:'https://github.com/Siraphob1/Find-your-hat' ,
+        linksite:'https://replit.com/@Siraphob2541/39SiraphobTopthpjsd5-find-your-hat#'
+    },
+    {   id: 10, 
+        topic:'Find your hat',
+        stack:['ReactJS' ,'Vite' ,'Tailwind'],
+        responsive:['Desktop'],
+        image:image10_ReactAssessmet,
+        isgif:true,
+        repoGithub:'https://github.com/Siraphob1/career-changer-react-assessment' ,
+        linksite:'https://siraphob-react-assessment.vercel.app/'
     }
 ]
