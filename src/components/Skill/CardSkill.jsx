0181e38bb7b73e25fed98830898156523378d31f
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-21 21:36:22 +0700

    create skillcomponent and card

--- src/components/Skill/CardSkill.jsx
+++ src/components/Skill/CardSkill.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
 const CardSkill = ({image , programLanguage}) => {
   return (
-    <div className=" w-[180px] py-[1rem] rounded-lg bg-[#18181b] bg-opacity-70 flex flex-col justify-center items-center hover:scale-[1.1] hover:cursor-pointer hover:delay-150 hover:duration-150 ">
+    <div className=" w-[180px] py-[1rem] rounded-lg bg-[#18181b] bg-opacity-70 flex flex-col justify-center items-center hover:scale-[1.1] hover:cursor-pointer delay-150 duration-150 ease-in-out ">
         <img src={image} alt="" className=" w-[50%]"/>
-        <p className=' font-bold text-[1.1rem] text-white opacity-70 '>{programLanguage}</p>
+        <p className=' font-semibold text-[1.1rem] text-white text-opacity-80 '>{programLanguage}</p>
     </div>
   )
 }
--- src/components/Skill/SkillComponent.jsx
+++ src/components/Skill/SkillComponent.jsx
@@ -1,18 +1,26 @@
+import TopicComponent from "../TopicComponent"
 import CardSkill from "./CardSkill"
 import { skillarray } from "./skillarray"
 
 
 const SkillComponent = () => {
   return (
-    <div id="skills" className=" min-h-screen bg-black bg-opacity-98 scroll-mt-[6rem] p-[2rem]">
-      <span className=" text-white px-[0.5rem] py-[0.2rem] rounded-lg hover:bg-[#515a5980] hover:cursor-pointer">#Skills</span>
-      <div className=" flex flex-wrap gap-[1rem]   w-[80%] mx-auto mt-[2rem] border ">
+    <div id="skills" className=" min-h-[90vh] bg-black bg-opacity-[98%] scroll-mt-[5rem] p-[2rem]">
+      <div className="w-[80%] mx-auto">
+        <TopicComponent topic={'Skill'}/>
+        <p className=" text-white pl-[0.5rem] text-opacity-80">These are the Tech Stacks I learned and used in my project.</p>
+
+        <div className=" flex flex-wrap gap-[1rem]    mt-[2rem] ">
         {skillarray.map((e)=>{
           return(
             <CardSkill key={e.id} image = {e.image}  programLanguage={e.text}/>
           )
         })}
-      </div>     
+      </div>    
+
+      </div>
+
+       
           
     </div>
   )
