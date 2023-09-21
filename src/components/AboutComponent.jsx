0697fb2a900966e3c4782d5219b4ad8950cb923d
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-21 21:35:25 +0700

    add filter background

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -8,11 +8,15 @@ const AboutComponent = () => {
             {/* Background */}
             <div className=" w-full h-[90vh] overflow-hidden relative">
                 <div style={{backgroundImage: `url("https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2lhdnk4MmRqOGVydTExbm0xbThuNmFieTh1NjgyNjR4eXpsMG01dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26tn33aiTi1jkl6H6/giphy.gif")` 
-                            ,backgroundRepeat: 'no-repeat'
-                            
+                            ,backgroundRepeat: 'no-repeat'                            
                             }}
-                        className=" w-[110%] h-[95vh] bg-cover bg-center blur-[5px] absolute top-[-1rem] left-[-1rem] ">     
+                        className=" w-[110%] h-[95vh] bg-cover bg-center  blur-[5px] absolute top-[-1rem] left-[-1rem] ">     
                 </div>
+            </div>
+
+            {/* Filter Background */}
+            <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-40">
+
             </div>
 
             {/* Content */}
@@ -23,7 +27,7 @@ const AboutComponent = () => {
                                 <h1 className=" text-[3rem] font-bold mb-[0.5rem]">
                                     Fullstack-Developer
                                 </h1>
-                                <p className=" text-[1rem]">
+                                <p className=" text-[1.2rem]">
                                     I am interested in coding as a Fullstack Developer. I decided to enroll in the Generation Fullstack camp to enhance my knowledge such as HTML5 CSS3 JS Boostrap5 Node.js Express.js MongoDB
                                 </p>
                             </div>
