0d07736e16904cd966545c6f9b6a484a1e017c2a
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-22 14:34:51 +0700

    useEducationlayout

--- src/components/education/EducationComponent.jsx
+++ src/components/education/EducationComponent.jsx
@@ -13,18 +13,18 @@ const EducationComponent = () => {
         </div>
       </EducationLayout>
 
-      <EducationLayout topic1={'Prince of Songkla University(PSU)'} topic2={'Bachelor of Engineering (Mechatronics Engineering)'} durationtime={'August 2018 - May 2022'} addresslearn={'Songkhla , Thailand'}>
+      <EducationLayout topic1={'Prince of Songkla University (PSU)'} topic2={'Bachelor of Engineering (Mechatronics Engineering)'} durationtime={'August 2018 - May 2022'} addresslearn={'Songkhla , Thailand'}>
         <div></div>
       </EducationLayout>
 
-      <EducationLayout topic1={'Benjamarachutit Pattani School(BM)'} topic2={''} durationtime={''} addresslearn={'Pattani , Thailand'}>
+      <EducationLayout topic1={'Benjamarachutit Pattani School (BM)'} topic2={''} durationtime={''} addresslearn={'Pattani , Thailand'}>
         <div className=" font-Incluesive-Sans">
           <section className=" flex justify-between text-white text-opacity-80">
-            <p className="">Enrichment Program of Science Mathematics Technology and Environment</p>
+            <p className="">Enrichment Program of Science Mathematics Technology and Environment (SMTE)</p>
             <span>2015 - 2018</span>
           </section>
           <section className=" flex justify-between text-white text-opacity-80">
-            <p className="">Science Math Environment MOU PSU</p>
+            <p className="">Science Math Environment MOU PSU (SME MOU)</p>
             <span>2013 - 2015</span>
           </section>
         </div>
