c3580f39d9124c1746d866c0065894f8417b4c5f
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-23 17:25:56 +0700

    move file Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import AboutComponent from "./components/AboutComponent"
-import ContactComponent from "./components/ContactComponent"
+import ContactComponent from "./components/contact/ContactComponent"
 import EducationComponent from "./components/education/EducationComponent"
 import ExperienceComponent from "./components/experience/ExperienceComponent"
 import IntroComponent from "./components/IntroComponent"
 import NavbarComponent from "./components/NavbarComponent"
 import SkillComponent from "./components/Skill/SkillComponent"
 import ProjectsComponent from "./components/project/ProjectsComponent"
+
 
 
 
