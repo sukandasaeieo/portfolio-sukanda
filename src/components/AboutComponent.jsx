82ed66b52870d33f7af7a9ca1bd644f4d980b75d
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-23 20:20:49 +0700

    responsive About

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -5,9 +5,9 @@ import LayoutComponent from "./LayoutComponent"
 const AboutComponent = () => {
   return (
     <LayoutComponent topic={'About'} idcomponent={'about'} bgopacity={'bg-opacity-[96%]'}>
-        <div className=" flex justify-between items-center mt-[2rem]">
-            <p className=" text-white text-[1.2rem] pl-[0.5rem] text-opacity-80  mt-[2rem] w-[40%] font-Incluesive-Sans">I am interested in coding as a Fullstack Developer. I decided to enroll in the Generation Fullstack camp to enhance my knowledge such as HTML5 CSS3 JS Boostrap5 Node.js Express.js MongoDB</p>
-            <img src="https://cdn.pixabay.com/photo/2015/09/05/22/33/office-925806_1280.jpg" alt="" className=" w-[50%] rounded-3xl" />
+        <div className=" flex flex-col-reverse lg:flex-row justify-between items-center mt-[2rem]">
+            <p className=" text-white lg:text-[1.2rem] pl-[0.5rem] text-opacity-80  mt-[2rem] lg:w-[40%] font-Incluesive-Sans">I am interested in coding as a Fullstack Developer. I decided to enroll in the Generation Fullstack camp to enhance my knowledge such as HTML5 CSS3 JS Boostrap5 Node.js Express.js MongoDB</p>
+            <img src="https://cdn.pixabay.com/photo/2015/09/05/22/33/office-925806_1280.jpg" alt="" className=" lg:w-[50%] rounded-3xl" />
         </div>
     </LayoutComponent>
   )
--- src/components/LayoutComponent.jsx
+++ src/components/LayoutComponent.jsx
@@ -2,11 +2,11 @@
 
 const LayoutComponent = ({children ,topic , idcomponent , bgopacity}) => {
   return (
-    <section id={idcomponent} className={` bg-black ${bgopacity} scroll-mt-[4rem] p-[2rem]`}>
+    <section id={idcomponent} className={` bg-black ${bgopacity} scroll-mt-[4rem] py-[2rem] lg:p-[2rem]`}>
         <div className="w-[80%] mx-auto">
 
             {/* Topic */}
-            <span className=" text-white text-[1.5rem] font-semibold font-Incluesive-Sans px-[0.5rem] py-[0.2rem] rounded-lg hover:bg-[#515a5980] hover:cursor-pointer">
+            <span className=" text-white text-[1.5rem] font-semibold font-Incluesive-Sans lg:px-[0.5rem] py-[0.2rem] rounded-lg hover:bg-[#515a5980] hover:cursor-pointer">
                 #{topic}
             </span>
 
