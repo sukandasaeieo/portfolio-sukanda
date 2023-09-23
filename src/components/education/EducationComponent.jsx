920f6024c47874b97efa02f41001082965ed6894
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-23 22:45:36 +0700

    responsive education

--- src/components/education/EducationComponent.jsx
+++ src/components/education/EducationComponent.jsx
@@ -7,11 +7,11 @@ const EducationComponent = () => {
     <LayoutComponent topic={'Education'} idcomponent={'education'} bgopacity={'bg-opacity-[98%]'}>
 
       <EducationLayout topic1={'Generation Thailand'} topic2={'Junior Software Developer Booscamp (JSD)'} durationtime={'July 2023 - October 2023'} addresslearn={'Online , Thailand'}>
-        <div className="text-white text-opacity-80 font-Incluesive-Sans pl-[2rem]">
-          <p>-Technical Skills: <span className=" ml-[0.7rem]">HTML / CSS / JS / ReactJS / NodeJS / ExpressJS / MongoDB / Git / GitHub</span></p>
-          <div className=" flex">
-            <p>-Soft Skills:</p>            
-            <p className=" ml-[0.7rem] max-w-[580px]">Growth Mindset / Future Orientation / Persistence / Personal Responsibility / Time Management / Teamwork / Orientation to Detail / Proactiveness</p>
+        <div className="text-white text-opacity-80 font-Incluesive-Sans text-[0.9rem] mt-[1rem] lg:mt-0 lg:text-[1rem] lg:pl-[2rem]">
+          <p>-Technical Skills: <span className=" ml-[0.7rem] ">HTML / CSS / JS / ReactJS / NodeJS / ExpressJS / MongoDB / Git / GitHub</span></p>
+          <div className=" mt-[1rem] lg:mt-0 lg:flex">
+            <p className=" inline">-Soft Skills:</p>            
+            <p className=" inline ml-[0.7rem] max-w-[580px]">Growth Mindset / Future Orientation / Persistence / Personal Responsibility / Time Management / Teamwork / Orientation to Detail / Proactiveness</p>
           </div>
         </div>
       </EducationLayout>
@@ -20,14 +20,9 @@ const EducationComponent = () => {
         <div></div>
       </EducationLayout>
 
-      <EducationLayout topic1={'Benjamarachutit Pattani School (BM)'} topic2={''} durationtime={''} addresslearn={'Pattani , Thailand'}>
-        <div className=" font-Incluesive-Sans">
-          <section className=" flex justify-between text-white text-opacity-80">
-            <p className="">Enrichment Program of Science Mathematics Technology and Environment (SMTE)</p>
-            <span>2015 - 2018</span>
-          </section>
-          
-        </div>
+      <EducationLayout topic1={'Benjamarachutit Pattani School (BM)'} topic2={'Enrichment Program of Science Mathematics Technology and Environment (SMTE)'} durationtime={'2015 - 2018'} addresslearn={'Pattani , Thailand'}>
+        <div></div>
+        
       </EducationLayout>
 
     </LayoutComponent>
