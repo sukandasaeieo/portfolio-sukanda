ce62cf6981cc227b92877aef25e7e7c5c132b318
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-23 20:14:58 +0700

    responsive

--- src/components/IntroComponent.jsx
+++ src/components/IntroComponent.jsx
@@ -17,14 +17,14 @@ const IntroComponent = () => {
             </div>
 
             {/* Content */}
-            <div className="flex items-center justify-center absolute top-0 left-0 w-full h-full ">
+            <div className="flex flex-col-reverse lg:flex-row items-center justify-center absolute top-0 left-0 box-border w-full h-full px-[1.5rem] ">
                         {/* Left */} 
-                        <section className=" text-white flex  items-center justify-center mr-[2rem]  ">
-                            <div className=" w-[500px] ">
-                                <h1 className=" text-[3rem] font-bold mb-[0.5rem]">
+                        <section className=" text-white flex  items-center justify-center mt-[2rem] lg:mt-0 lg:mr-[2rem]  ">
+                            <div className=" lg:w-[500px] ">
+                                <h1 className=" text-[8vw] sm:text-[6.5vw] md:text-[6vw]  lg:text-[3rem] font-bold mb-[0.5rem]">
                                     Fullstack-Developer
                                 </h1>
-                                <p className=" text-[1.2rem] font-Incluesive-Sans">
+                                <p className=" lg:text-[1.2rem] font-Incluesive-Sans">
                                     Hi, I am 
                                     <span className=" text-yellow-400"> &ldquo;Top&rdquo;</span> or  
                                     <span className=" text-yellow-400"> &ldquo;Siraphob Reanmanorom.&rdquo; </span>
@@ -35,7 +35,7 @@ const IntroComponent = () => {
 
                         {/* Right */}                
                         <section>
-                            <img src="https://1417094351.rsc.cdn77.org/articles/9940/9939350/thumbnail/large.gif?4" alt="" className=" w-[400px] rounded-full" />
+                            <img src="https://1417094351.rsc.cdn77.org/articles/9940/9939350/thumbnail/large.gif?4" alt="" className=" w-[40vw] lg:w-[400px] rounded-full" />
                         </section>
             </div>
     </section>
