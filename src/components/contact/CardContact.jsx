7e7695cb96d2b102ddf588822d7c6c35d3e48676
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-24 12:29:32 +0700

    Contact responsive

--- src/components/contact/CardContact.jsx
+++ src/components/contact/CardContact.jsx
@@ -3,9 +3,9 @@
 const CardContact = ({icon , nameicon ,linktarget }) => {
   return (
     <a  href={linktarget} target="_blank" rel="noopener noreferrer" 
-        className="btn max-w-[12rem]    flex justify-start items-center gap-x-[0.7rem]">
+        className="btn w-auto sm:w-auto  sm:max-w-[12rem]    sm:flex  sm:justify-start sm:items-center sm:gap-x-[0.7rem] active:bg-base-300">
         {icon}
-        <span className=" normal-case">{nameicon}</span>
+        <span className=" hidden sm:block normal-case">{nameicon}</span>
        
     </a>
   )
--- src/components/contact/ContactComponent.jsx
+++ src/components/contact/ContactComponent.jsx
@@ -50,41 +50,43 @@ const ContactComponent = () => {
   return (
     <LayoutComponent topic={'Contact'} idcomponent={'contact'} bgopacity={'bg-opacity-[98%]'}>
         {/* Label */}
-        <p className=" text-white pl-[0.5rem] text-opacity-80 font-Incluesive-Sans">Please feel free to contact me via the provided form or any other available link.</p>
+        <p className=" text-white text-[0.9rem] lg:text-[1rem] pl-[0.5rem] text-opacity-80 font-Incluesive-Sans">Please feel free to contact me via the provided form or any other available link.</p>
 
         {/* Contact */}
-        <div className=" bg-[#18181b] rounded-2xl p-[1rem] flex justify-between items-center mt-[2rem]  " >
+        <div className=" bg-[#18181b] rounded-2xl p-[1rem] flex flex-col-reverse sm:flex-row sm:justify-between items-center mt-[2rem] gap-y-[1rem] sm:gap-y-0  " >
 
             {/* Left */}
-            <section className=" w-[40%] flex justify-center">
+            <section className=" sm:w-[40%] flex justify-center">
               <div className=" flex flex-col gap-y-[1rem]">
                 <span className=" text-center text-white text-opacity-70">Contact by</span>
-                <CardContact icon={<BsLinkedin className=" text-[1.1rem]"/>} nameicon={'Linkedin'} linktarget={'https://www.linkedin.com/in/siraphob-reanmanorom/'}/>
-                <CardContact icon={<HiOutlineMail className=" text-[1.1rem]"/>} nameicon={'Email'} linktarget={'mailto:siraphobtop95@gmail.com?subject=contact from Portfolio Siraphob'}/>
-                <CardContact icon={<BsGithub className=" text-[1.1rem]"/>} nameicon={'GitHub'} linktarget={'https://github.com/Siraphob1'}/>
-                <CardContact icon={<SiFrontendmentor className=" text-[1.1rem]"/>} nameicon={'Frontend Mentor'} linktarget={'https://www.frontendmentor.io/profile/Siraphob1'}/>
+                <div className=" flex sm:flex-col sm:gap-y-[1rem] gap-x-[1rem] sm:gap-x-0">
+                <CardContact icon={<BsLinkedin className=" text-[1.2rem] sm:text-[1.1rem]"/>} nameicon={'Linkedin'} linktarget={'https://www.linkedin.com/in/siraphob-reanmanorom/'}/>
+                <CardContact icon={<HiOutlineMail className="text-[1.5rem] sm:text-[1.1rem]"/>} nameicon={'Email'} linktarget={'mailto:siraphobtop95@gmail.com?subject=contact from Portfolio Siraphob'}/>
+                <CardContact icon={<BsGithub className=" text-[1.4rem] sm:text-[1.1rem]"/>} nameicon={'GitHub'} linktarget={'https://github.com/Siraphob1'}/>
+                <CardContact icon={<SiFrontendmentor className="text-[1.2rem] sm:text-[1.1rem]"/>} nameicon={'Frontend Mentor'} linktarget={'https://www.frontendmentor.io/profile/Siraphob1'}/>
+                </div>
               </div>
             </section>
 
             {/* Center */}
-            <span className=" w-[10%] text-white text-opacity-70">or</span>
+            <span className=" sm:w-[10%] text-white text-opacity-70">or</span>
 
             {/* Right */}
-            <section className=" w-[50%]">              
+            <section className=" sm:w-[50%]">              
               <form className=" flex flex-col gap-y-[1rem] text-white text-opacity-70" onSubmit={(e)=>submitHandler(e)}>
 
                 {/* Form Top */}
-                <section className=" flex gap-x-[1rem] ">
+                <section className=" flex gap-x-[1rem] text-[0.9rem] sm:text-[1rem] ">
                   {/* Input Left */}
                   <section className="form-control w-full max-w-xs">
                     <label htmlFor="contact-name" className="label">
-                      <span className="label-text text-white text-[1rem]  text-opacity-70">Your name</span>
+                      <span className="label-text text-white text-opacity-70">Your name</span>
                     </label>
                     <input  id="contact-name" 
                             type="text" 
                             ref={refName}
                             placeholder="Name" 
-                            className="input input-bordered w-full max-w-xs text-black" 
+                            className="input input-bordered w-full max-w-xs text-black text-[0.85rem] sm:text-[1rem]" 
                             onChange={(e)=>setNamevisitor(e.target.value)}
                             value={namevisitor}
                             />
@@ -93,13 +95,13 @@ const ContactComponent = () => {
                   {/* Input Right */}
                   <section className="form-control w-full max-w-xs">
                     <label htmlFor="contact-email" className="label">
-                      <span className="label-text text-white text-[1rem]  text-opacity-70">Your email</span>
+                      <span className="label-text text-white text-opacity-70">Your email</span>
                     </label>
                     <input  id="contact-email" 
                             type="text" 
                             ref={refEmail}
                             placeholder="Email" 
-                            className="input input-bordered w-full max-w-xs text-black" 
+                            className="input input-bordered w-full max-w-xs text-black text-[0.85rem] sm:text-[1rem]" 
                             onChange={(e)=>setEmailvisitor(e.target.value)}
                             value={emailvisitor}
                             />
@@ -109,13 +111,13 @@ const ContactComponent = () => {
                 </section>
 
                 {/* Form Bottom */}                
-                <section className="form-control">
+                <section className="form-control text-[0.9rem] sm:text-[1rem]">
                   <label htmlFor="contact-message"  className="label">
-                    <span className="label-text text-white text-[1rem]  text-opacity-70">Your Message</span>
+                    <span className="label-text text-white text-opacity-70">Your Message</span>
                   </label>
                   <textarea   id="contact-message" 
                               ref={refMessage}
-                              className="textarea textarea-bordered h-24 text-black " 
+                              className="textarea textarea-bordered h-24 text-black text-[0.85rem] sm:text-[1rem] " 
                               placeholder="Message"
                               onChange={(e)=>setMessagevisitor(e.target.value)}
                               value={messagevisitor}
@@ -124,8 +126,8 @@ const ContactComponent = () => {
                 </section>
 
                 {/* Button */}
-                <section className=" flex items-center gap-x-[1rem]">
-                <button type='submit' className="btn btn-neutral hover:brightness-[170%]  max-w-[200px] normal-case " onClick={()=>setIscliked(true)}>Send Message <LuSend className=" text-[1.2rem]"/></button>
+                <section className=" flex flex-col sm:flex-row sm:items-center gap-x-[1rem] gap-y-[0.5rem] sm:gap-y-0">
+                <button type='submit' className="btn btn-neutral hover:brightness-[170%]  max-w-[200px] normal-case  " onClick={()=>setIscliked(true)}>Send Message <LuSend className=" text-[1.2rem]"/></button>
                 
                 {!isclicked ? null 
                            : sendsuccess  ? <p className=" text-green-500">The email has been successfully sent.</p>
