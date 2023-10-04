0bbae651df3d411f6886d17a28c0505132eea8fc
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-05 03:15:50 +0700

    add sending email

--- src/components/contact/ContactComponent.jsx
+++ src/components/contact/ContactComponent.jsx
@@ -5,6 +5,7 @@ import CardContact from "./CardContact";
 import { BsLinkedin ,BsGithub } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
 import { SiFrontendmentor } from "react-icons/si";
+import axios from "axios";
 
 const ContactComponent = () => {
   const [namevisitor , setNamevisitor] = useState(''); 
@@ -12,7 +13,9 @@ const ContactComponent = () => {
   const [messagevisitor , setMessagevisitor] = useState('');
 
   const [isclicked , setIscliked] = useState(false);
-  const [sendsuccess , setSendsuccess] = useState(false);
+  const [completeFill , setCompleteFill] = useState(false);
+  const [isSending , setIsSending] = useState(false);
+  const [sendSuccess , setSendSuccess] = useState(false);
 
   const refName = useRef();
   const refEmail = useRef();
@@ -20,32 +23,56 @@ const ContactComponent = () => {
 
   useEffect(()=>{
     setIscliked(false)
-    setSendsuccess(false)
+    setCompleteFill(false)
   }, [namevisitor , emailvisitor , messagevisitor])
 
 
-  const CheckInput =() =>{
-    namevisitor ? null : refName.current.focus();    
-    if(namevisitor){
-      emailvisitor ? null : refEmail.current.focus();
-    }
-    if(namevisitor && emailvisitor){
-      messagevisitor ? null : refMessage.current.focus();
-    }
-    if(namevisitor && emailvisitor && messagevisitor){
-      setSendsuccess(true)
-  }
-  }
+  
 
   const submitHandler = (e) =>{
-    e.preventDefault()
+    e.preventDefault()           
+       
+    detectEmptyInput();   
+}
 
-    CheckInput();
-    if(sendsuccess){
-      console.log('send success')
-    }
+  const detectEmptyInput = () =>{
+    namevisitor ? null : refName.current.focus();    
     
-  }
+    if(namevisitor){
+      emailvisitor ? null : refEmail.current.focus();      
+    }
+    if(namevisitor && emailvisitor){
+      messagevisitor ? null : refMessage.current.focus();      
+    }
+    if(namevisitor && emailvisitor && messagevisitor){
+      setCompleteFill(true)      
+      sendEmail()
+    }
+  
+}
+
+  const sendEmail = async () => {
+  const url = 'http://localhost:3500/sendmail'
+
+    const objsend = {
+      namevisitor , 
+      emailvisitor ,
+      messagevisitor
+    }
+    setIsSending(true);
+    try {
+      const response = await axios.post( url , objsend)
+      if(response.statusText === 'OK'){
+        setIsSending(false);
+        setSendSuccess(true);      
+      }
+    } catch (error) {
+      setIsSending(false);
+      setSendSuccess(false);  
+    }
+}
+
+
 
   return (
     <LayoutComponent topic={'Contact'} idcomponent={'contact'} bgopacity={'bg-opacity-[98%]'}>
@@ -55,7 +82,7 @@ const ContactComponent = () => {
         {/* Contact */}
         <div className=" bg-[#18181b] rounded-2xl p-[1rem] flex flex-col-reverse sm:flex-row sm:justify-between items-center mt-[2rem] gap-y-[1rem] sm:gap-y-0  " >
 
-            {/* Left */}
+            {/* Contact Left */}
             <section className=" sm:w-[40%] flex justify-center">
               <div className=" flex flex-col gap-y-[1rem]">
                 <span className=" text-center text-white text-opacity-70 hidden sm:block">Contact by</span>
@@ -68,10 +95,10 @@ const ContactComponent = () => {
               </div>
             </section>
 
-            {/* Center */}
+            {/* Contact Center */}
             <span className=" sm:w-[10%] text-white text-opacity-70">or</span>
 
-            {/* Right */}
+            {/* Contact Right */}
             <section className=" w-full sm:w-[50%]">              
               <form className=" flex flex-col sm:gap-y-[1rem] text-white text-opacity-70" onSubmit={(e)=>submitHandler(e)}>
 
@@ -105,9 +132,7 @@ const ContactComponent = () => {
                             onChange={(e)=>setEmailvisitor(e.target.value)}
                             value={emailvisitor}
                             />
-                  </section>
-
-                  
+                  </section>                  
                 </section>
 
                 {/* Form Bottom */}                
@@ -129,9 +154,14 @@ const ContactComponent = () => {
                 <section className=" flex flex-col lg:flex-row lg:items-center gap-x-[1rem] gap-y-[0.5rem] sm:gap-y-0 text-[0.9rem] sm:text-[1rem]">
                 <button type='submit' className="btn btn-neutral hover:brightness-[170%]  max-w-[200px] normal-case mt-[1rem] sm:mt-0" onClick={()=>setIscliked(true)}>Send Message <LuSend className=" text-[1.2rem]"/></button>
                 
+                {/* Show result */}
                 {!isclicked ? null 
-                           : sendsuccess  ? <p className=" text-green-500">The email has been successfully sent.</p>
-                                          : <p className=" text-red-400">Please ensure that you provide all the required information in full.</p>
+                           : !completeFill  ? <p className=" text-red-400">Please ensure that you provide all the required information in full.</p>
+                                            : isSending  ? <p className="flex items-center">sending <span className="loading loading-dots loading-xs ml-[0.5rem]"></span></p>
+                                                          : !sendSuccess  ? <p className=" text-red-400">The attempt to send an email has failed.</p>
+                                                                          : <p className=" text-green-500">The email has been successfully sent.</p>
+                                           
+                                          
                 }
                 </section>
                 
