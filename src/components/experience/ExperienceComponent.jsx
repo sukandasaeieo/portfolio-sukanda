328a4e778fde2412b0bb7b6e70606f2f44dd6237
sukandasaeieo <sukandasaeieo@gmail.com>
2024-12-24 19:13:52 +0700

    updated

--- src/components/experience/ExperienceComponent.jsx
+++ src/components/experience/ExperienceComponent.jsx
@@ -6,44 +6,29 @@ const ExperienceComponent = () => {
   return (
     <LayoutComponent topic={'Experience'} idcomponent={'experience'} bgopacity={'bg-opacity-[90%]'}>
       
-      <ExperienceLayout topic1={'Krissana Engineering & Supply'} topic2={'Administrate Officer'} durationtime={'December 2021 - March 2023'} addresslearn={'Bangkok , Thailand'}>
-        <p>- Monitoring payments and preparing invoices and purchase orders</p>
-        <p>- Conducts meetings as required to resolve questions or to discuss changes proposed by the Owner or Contractor</p>
-        <p>- Provide technical assistance and answer inquiries for professional staff, contractors and the general public</p>  
-        <p>- Prepare and maintain reports, forms, spreadsheets, databases and related correspondence to ensure compliance with contract documents and specifications</p>      
+      <ExperienceLayout topic1={'W&J Engineering'} topic2={'IT Project Coordinator'} durationtime={'June 2024 - November 2024'} addresslearn={'Bangkok , Thailand'}>
+        <p>- Attend meetings and conduct testing with the local Thai team; assist testers when they need to communicate with the development team and the Project Manager, including translating between Thai and English as needed</p>
+        <p>- Monitor and follow up on the work scope and Jira tasks to ensure the project remains on track and aligns with planned objectives</p>
+        <p>- Assign tickets to testing team and collaborate with them during the testing process</p>  
+        <p>- Gather and document requirements, ensuring effective communication with the Project Manager and development team</p>  
+        <p>Prepare and distribute detailed meeting minutes for the Project Manager</p>   
+        <p>Conduct End-to-End (E2E) testing and User Acceptance Testing (UAT) </p>
+        <p>Provide training and implement related processes</p>
+        <p>Prepare and deliver project progress reports, official correspondence, and other related project documentation </p>
+        <p>Provide regular updates and progress reports to stakeholders</p>
       </ExperienceLayout>
       
-      <ExperienceLayout topic1={'Digital Economy Promotion Agency'} topic2={'Customer Relations Officer'} durationtime={'October 2020 - November 2021'} addresslearn={'Bangkok , Thailand'}>
-        <p>- Offering assistance to customers to ensure they find the right product/service</p>
-        <p>- Communicating with customers by email, phone, and face-to-face</p>
-        <p>- Refer customers to senior staff when necessary</p> 
-        <p>- Learning about company's products/services and remaining up to date with any changes</p>    
-        <p>- Investigating and solving customer problems</p>
-        <p>- Meeting with management to discuss possible improvements to the company's customer service</p>  
+      <ExperienceLayout topic1={'Digital Economy Promotion Agency'} topic2={'Customer Relations Officer'} durationtime={'October 2020 - November 2021'} addresslearn={'Bangkok , Thailand'}>  
       </ExperienceLayout>
 
-      <ExperienceLayout topic1={'Lucky Global Corporation (Thailand)'} topic2={'Executive Secretary'} durationtime={'2016 - 2017'} addresslearn={'Bangkok , Thailand'}>
-        <p>- Managing daily administrative functions conferences etc.</p>
-        <p>- Attend meetings and keep minutes</p>
-        <p>- Receive and screen phone calls and redirect them when appropriate</p> 
-        <p>- Handle and prioritize all outgoing or incoming correspondence (e-mail, letters, packages etc.)</p>  
-        <p>- Make travel arrangements for executives</p>    
-        <p>- Handle confidential documents ensuring they remain secure</p> 
+      <ExperienceLayout topic1={'AuPair SA'} topic2={'Aupair/Nanny'} durationtime={'2019 - 2020'} addresslearn={'South Africa'}> 
       </ExperienceLayout>
 
-      <ExperienceLayout topic1={'NEC Corporation (Thailand)'} topic2={'Receptionist & Operator'} durationtime={'2015 - 2016'} addresslearn={'Bangkok , Thailand'}>
-        <p>- Welcomes visitors by greeting them, in person or on the telephone and answering or referring inquiries</p>
-        <p>- Directs visitors by maintaining employee and department directories</p>
-        <p>- Maintains safe and clean reception area by complying with procedures, rules, and regulations maintains continuity among work teams by documenting and communicating actions, irregularities, and continuing needs</p>        
-        <p>- Contributes to team effort by accomplishing related results as needed</p>
+      <ExperienceLayout topic1={'NEC Corporation (Thailand)'} topic2={'Executive Secretary'} durationtime={'2016 - 2018'} addresslearn={'Bangkok , Thailand'}>        
       </ExperienceLayout>
 
 
-      <ExperienceLayout topic1={'Sawasdee Hotel Banglumpoo Inn 2'} topic2={'Receptionist'} durationtime={'2014 - 2015'} addresslearn={'Bangkok , Thailand'}>
-        <p>- Welcome and Check in- out Guests</p>
-        <p>- Handle Guest Inquiries and Complaints</p>
-        <p>- Answer Phones</p>    
-        <p>- Process Payments</p>    
+      <ExperienceLayout topic1={'NEC Corporation (Thailand) Ltd.'} topic2={'Receptionist & Operator'} durationtime={'2014 - 2016'} addresslearn={'Bangkok , Thailand'}>    
       </ExperienceLayout>
 
     </LayoutComponent>
